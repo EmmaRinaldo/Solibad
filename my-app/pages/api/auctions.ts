992da57b676eeafa044ffd2e24863df6ed080bc2
Quegrec <78724000+Quegrec@users.noreply.github.com>
2025-01-07 12:12:11 +0100

    Add find singular auction by id

--- my-app/pages/api/auctions.ts
+++ my-app/pages/api/auctions.ts
@@ -6,22 +6,37 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'GET') {
-        try {
-            const auctions = await prisma.auction.findMany();
-            res.status(200).json(auctions);
-        } catch (error) {
-            res.status(500).json({ error: 'Erreur lors de la récupération des enchères' });
+        const { id } = req.query;
+        if (id) {
+            try {
+                const auction = await prisma.auction.findUnique({
+                    where: { id: String(id) }
+                });
+                if (auction) {
+                    res.status(200).json(auction);
+                } else {
+                    res.status(404).json({ error: 'Enchère non trouvée' });
+                }
+            } catch (error) {
+                res.status(500).json({ error: 'Erreur lors de la récupération de l\'enchère' });
+            }
+        } else {
+            try {
+                const auctions = await prisma.auction.findMany();
+                res.status(200).json(auctions);
+            } catch (error) {
+                res.status(500).json({ error: 'Erreur lors de la récupération des enchères' });
+            }
         }
     } else if (req.method === 'POST') {
         try {
             const newAuction = await prisma.auction.create({
-                data: 
-                    {
-                        title: req.body.title,
-                        description: req.body.description,
-                        startPrice: req.body.startPrice,
-                        minIncr: req.body.minIncr
-                    }
+                data: {
+                    title: req.body.title,
+                    description: req.body.description,
+                    startPrice: req.body.startPrice,
+                    minIncr: req.body.minIncr
+                }
             });
             res.status(201).json(newAuction);
         } catch (error) {
