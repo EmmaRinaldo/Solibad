9b7327542bcc27f9917acb97b9a49788cbe7663d
Anaïs Mouelhi <117044920+Anais-mouelhi@users.noreply.github.com>
2025-01-10 10:05:42 +0100

    Update HeroSection.jsx

--- my-app/components/HeroSection.jsx
+++ my-app/components/HeroSection.jsx
@@ -27,7 +27,13 @@ const Header = () => {
               </p>
               <div className="flex flex-col gap-4 sm:flex-row justify-center lg:justify-start">
                 <Link href="#active-auctions">
-                  <button className="inline-flex items-center justify-center rounded-md border border-[#020817]/80 px-8 py-3 text-sm font-medium text-[#020817] shadow-md hover:bg-blue-700 transition">
+                  <button className="inline-flex items-center justify-center rounded-md border border-[#020817]/80 px-8 py-3 text-sm font-medium text-[#020817] shadow-md hover:bg-black hover:text-white transition" style={{
+      borderWidth: "3px", // Épaisseur de la bordure
+      borderStyle: "solid", // Style de la bordure
+      borderRadius: "12px", // Coins arrondis
+      borderImageSource: `linear-gradient(90deg, rgba(229,65,41,1) 0%, rgba(234,111,17,1) 10%, rgba(255,231,24,1) 25%, rgba(136,187,21,1) 40%, rgba(53,159,43,1) 55%, rgba(19,161,174,1) 70%, rgba(16,139,200,1) 85%, rgba(13,56,137,1) 100%)`,
+      borderImageSlice: 1, // Utilisation correcte du dégradé
+    }}>
                     Commencer à enchérir
                   </button>
                 </Link>
